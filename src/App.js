352ddfa9f6160ee58382eb310f00b3f575fc5535
Orhmo <oorhmo@gmail.com>
2023-02-02 15:34:55 +0100

    Dashboard Update

--- src/App.js
+++ src/App.js
@@ -10,20 +10,27 @@ import Auth from './pages/Sign-in'
 import Dashboard from './pages/Dashboard'
 
 import Nav from './components/SideBar'
+import Top from './components/TopBar'
 import Stat from './components/Stat'
 
 const App = () => {
   return (
     <div className="App">
-      <Router>
-        <Nav />
-        <Stat/>
+    <Router>
+    <div className ="side">
+
+      <Nav />
+      <Stat />
+    </div>
+      <main className="content">
+        <Top />
         <Routes>
           <Route path="/"
             exact element={<Auth />} />
           <Route path="/dashboard"
             exact element={<Dashboard />} />
         </Routes>
+        </main>
       </Router>
     </div>
   );
